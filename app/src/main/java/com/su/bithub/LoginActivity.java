cefd66c8a8cb5bc772ad5ca58857db10399d77de
shivamupadhyay1022@gmail.com <luke_sk_w@lker@4261>
2022-08-23 02:54:19 +0530

    v0.0.4(23/08/22)

--- app/src/main/java/com/su/bithub/LoginActivity.java
+++ app/src/main/java/com/su/bithub/LoginActivity.java
@@ -6,19 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
+
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
-import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,34 +41,48 @@ public class LoginActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
+        FirebaseUser currentUser = auth.getCurrentUser();
+        if(currentUser != null){
+            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        }else {
+            login.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String txt_email    = email.getText().toString().trim();
+                    String txt_password = password.getText().toString().trim();
 
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String txt_email    = email.getText().toString().trim();
-                String txt_password = password.getText().toString().trim();
-
-                loginuser(txt_email,txt_password);
-            }
-        });
-
+                    loginuser(txt_email,txt_password);
+                }
+            });
+        }
     }
 
     private void loginuser(String email, String password) {
 
-        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
-                    finish();
-                }else{
-                    Toast.makeText(LoginActivity.this, "Login UnSuccessful", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                            startActivity(new Intent(LoginActivity.this,MainActivity.class));
+                            Toast.makeText(LoginActivity.this, "Login Successfull",Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException){
+                                Toast.makeText(LoginActivity.this, "Invalid Credentials.",Toast.LENGTH_SHORT).show();
+                            }else if (task.getException() instanceof FirebaseAuthInvalidUserException){
+                                Toast.makeText(LoginActivity.this, "Invalid user.",Toast.LENGTH_SHORT).show();
+                            }else if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(LoginActivity.this, "User Collision.",Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(LoginActivity.this, "Auth error.",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                });
 
     }
 }
--- app/src/main/java/com/su/bithub/RegisterActivity.java
+++ app/src/main/java/com/su/bithub/RegisterActivity.java
@@ -3,11 +3,9 @@ package com.su.bithub;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,9 +14,15 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -34,7 +38,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText name;
     private EditText sec;
     private EditText semester;
-    private EditText year;
+    private EditText admyear;
     private Button register;
 
     private FirebaseAuth auth;
@@ -48,37 +52,37 @@ public class RegisterActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         register = findViewById(R.id.register1);
-        name     = findViewById(R.id.login_name);
-        sec      = findViewById(R.id.login_section);
-        semester = findViewById(R.id.login_semester);
-        year     = findViewById(R.id.login_year);
+        name = findViewById(R.id.login_name);
+        sec = findViewById(R.id.login_section);
+        admyear = findViewById(R.id.login_admyear);
 
         auth = FirebaseAuth.getInstance();
 
-        List<String> states= Arrays.asList("CS","IT","ECE","Mechanical","Chemical","Production","QEDS");
-        final Spinner spinner=findViewById(R.id.login_branch);
-        ArrayAdapter adapter=new ArrayAdapter(RegisterActivity.this,android.R.layout.simple_spinner_item,states);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        List<String> branches = Arrays.asList("CS", "IT", "ECE", "Mechanical", "Chemical", "Production", "QEDS");
+        final Spinner spinner1 = findViewById(R.id.login_branch);
+        ArrayAdapter adapter1 = new ArrayAdapter(RegisterActivity.this, android.R.layout.simple_spinner_item, branches);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        List<String> years = Arrays.asList("1st Year", "2nd Year", "3rd Year", "4th Year", "5th Year", "6th Year", "7th Year", "8th Year");
+        final Spinner spinner2 = findViewById(R.id.login_branch);
+        ArrayAdapter adapter2 = new ArrayAdapter(RegisterActivity.this, android.R.layout.simple_spinner_item, years);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
 
         db = FirebaseFirestore.getInstance();
-
-
 
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String txt_email    = email.getText().toString().trim();
+                String txt_email = email.getText().toString().trim();
                 String txt_password = password.getText().toString().trim();
-                String branch       = spinner.getSelectedItem().toString();
-                String txt_name     = name.getText().toString();
-                String txt_section  = sec.getText().toString();
+                String branch = spinner1.getSelectedItem().toString();
+                String txt_name = name.getText().toString().trim();
+                String txt_section = sec.getText().toString().trim();
                 String txt_semester = semester.getText().toString();
-
-
-
-
+                String yearno  = spinner2.getSelectedItem().toString();
 
 
                 if (TextUtils.isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
@@ -86,37 +90,59 @@ public class RegisterActivity extends AppCompatActivity {
                 } else if (txt_password.length() < 6) {
                     Toast.makeText(RegisterActivity.this, "Password too short!", Toast.LENGTH_SHORT).show();
                 } else {
-                    registerUser(txt_email, txt_password, branch, txt_name,txt_section,txt_semester);
+                    registerUser(txt_email, txt_password, branch, txt_name, txt_section, txt_semester,yearno);
                 }
             }
         });
     }
 
-    private void registerUser(String email, String password, String branch, String name, String section, String semester) {
+    private void registerUser(String email, String password, String branch, String name, String section, String semester, String yearno) {
 
-        String Year = year.getText().toString();
+        String ADMYear = admyear.getText().toString();
 
-        Map<String,Object> info= new HashMap<>();
+        Map<String, Object> info = new HashMap<>();
 
-        info.put("Email",email);
-        info.put("Password",password);
-        info.put("Name",name);
-        info.put("Branch",branch);
-        info.put("Section",section);
-        info.put("Semester",semester);
+        info.put("Email", email);
+        info.put("Password", password);
+        info.put("Name", name);
+        info.put("Branch", branch);
+        info.put("Section", section);
+        info.put("Semester", semester);
+        info.put("Admission Year", ADMYear);
+        info.put("Id", auth.getCurrentUser().getUid());
 
-        db.collection("1 Year").document(name).set(info);
+        db.collection(yearno).document(name)
+                .set(info)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(RegisterActivity.this,MainActivity.class));
+                            Toast.makeText(RegisterActivity.this, "Updated Successfully",Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(RegisterActivity.this, "User with same email alreday Exits",Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(RegisterActivity.this, "Update error.",Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                });
+
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     // Sign in success, update UI with the signed-in user's information
-                    Toast.makeText(RegisterActivity.this, "Authentication success.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Registered successfully.", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(RegisterActivity.this, MainActivity.class));
 
                 } else {
                     // If sign in fails, display a message to the user.
-                    Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegisterActivity.this, "Registration failed.", Toast.LENGTH_SHORT).show();
 
                 }
             }
