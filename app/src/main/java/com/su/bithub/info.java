c8a9c1649852262b8b59baeef59f588739e513e7
shivamupadhyay1022@gmail.com <luke_sk_w@lker@4261>
2022-08-23 02:54:19 +0530

    v0.0.4(23/08/22)

---
+++ app/src/main/java/com/su/bithub/info.java
@@ -0,0 +1,12 @@
+package com.su.bithub;
+
+public class info {
+
+    public String Gemail    = "NA";
+    public String Gname     = "NA";
+    public String Gbranch   = "NA";
+    public String Gsem      = "NA";
+    public String Gyear     = "NA";
+
+
+}
